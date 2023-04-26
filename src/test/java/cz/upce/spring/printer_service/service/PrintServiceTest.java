fee8c6f129d0e99575feadc5cf2e684096774d86
Karel Andres <kajusuandr97@gmail.com>
2023-04-26 23:39:32 +0200

    Testy

---
+++ src/test/java/cz/upce/spring/printer_service/service/PrintServiceTest.java
@@ -0,0 +1,29 @@
+package cz.upce.spring.printer_service.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+public class PrintServiceTest {
+
+    private PrintService printService;
+
+    @BeforeEach
+    public void setUp(){
+        printService = new PrintService();
+    }
+
+    @Test
+    public void testPrint(){
+
+    }
+
+    @Test
+    public void testCheckPrinters(){
+
+    }
+}
---
+++ src/test/java/cz/upce/spring/terminal_service/service/TerminalServiceTest.java
@@ -0,0 +1,10 @@
+package cz.upce.spring.terminal_service.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+public class TerminalServiceTest {
+}
